5e7fcdda4eb3af4b8129eb79c07a550e64cbaabc
Lobster <sato4ebi@gmail.com>
2023-12-21 14:31:19 +0900

    K22024画像アップロードプログラム
    アップロードした画像はstaticディレクトリ下のuploadsディレクトリに追加されていきます。

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        if not os.path.exists(app.config['UPLOAD_FOLDER']):
+            os.makedirs(app.config['UPLOAD_FOLDER'])
+
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(file_path)
+        return render_template('index.html', filename=file.filename)
+
+    return redirect(request.url)
+
+#http://127.0.0.1:5000/
+if __name__ == '__main__':
+    app.run(debug=True)
