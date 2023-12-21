f45f5976eab4d47bfd9dbc43c83662c6ffbc3281
k22044 <153164749+k22044@users.noreply.github.com>
2023-12-21 16:11:25 +0900

    一覧の追加

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# main.py
 from flask import Flask, render_template, request, redirect, url_for
 import os
 
@@ -8,9 +9,29 @@ app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
+def create_directory(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+@app.before_request
+def initialize():
+    create_directory(app.config['UPLOAD_FOLDER'])
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/uploaded_images')
+def show_uploaded_images():
+    images = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('uploaded_images.html', images=images)
+
+@app.route('/processed_images')
+def show_processed_images():
+    # 画像処理済み画像の一覧を取得するロジックをここに追加
+    processed_images = os.listdir('static/processed')  # 画像処理済み画像のディレクトリを指定
+
+    return render_template('processed_images.html', images=processed_images)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -32,6 +53,5 @@ def upload_file():
 
     return redirect(request.url)
 
-#http://127.0.0.1:5000/
 if __name__ == '__main__':
     app.run(debug=True)
